d9c0cb8089825dce764090836bebb957db17f206
Kolmassammas <Kolmassammas@gmail.com>
2021-05-12 12:48:56 +0300

    upgraded main.py

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ import random
 
 
 def main():
-    
-    
-    
-    
-    
-    pass
 
+    jarj = []
+    with open("russianwords.txt", "r", encoding="UTF8") as f:
+        for line in f:
+            line = line.strip()
+            jarj.append(line)
 
+    jarj2 = []
+    with open("englishwords.txt", "r", encoding="UTF8") as f2:
+        for line in f2:
+            line = line.strip()
+            jarj2.append(line)
+            
+            
+    choicenumber = random.randint(0, len(jarj))
+    
+    print(jarj[choicenumber])
+    print(jarj2[choicenumber])
 
-
+    
+    
+    
+    
+    
 if __name__ == "__main__":
     main()
