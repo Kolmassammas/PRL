971d2bd3bfaf3fc3c08a0872dc9fc087a14b2c55
Kolmassammas <Kolmassammas@gmail.com>
2021-05-12 13:37:33 +0300

    each second spamming two words in eng and rus

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import random
+import PySimpleGUI as sg
+from time import sleep
 
-
-def main():
-
-    jarj = []
-    with open("russianwords.txt", "r", encoding="UTF8") as f:
-        for line in f:
-            line = line.strip()
-            jarj.append(line)
-
+def getEnglishWord(choicenumber):
     jarj2 = []
     with open("englishwords.txt", "r", encoding="UTF8") as f2:
         for line in f2:
             line = line.strip()
             jarj2.append(line)
-            
-            
-    choicenumber = random.randint(0, len(jarj))
-    
-    print(jarj[choicenumber])
-    print(jarj2[choicenumber])
 
-    
-    
-    
-    
-    
+    return jarj2[choicenumber]
+
+
+def getRussianWord(choicenumber):
+    jarj = []
+    with open("russianwords.txt", "r", encoding="UTF8") as f:
+        for line in f:
+            line = line.strip()
+            jarj.append(line)
+        
+    return jarj[choicenumber]
+
+
+def main():
+    while True:
+        choicenumber = random.randint(0, 9197)
+        print(getRussianWord(choicenumber))
+        print(getEnglishWord(choicenumber))
+        sleep(1)
+
+
+
 if __name__ == "__main__":
     main()
